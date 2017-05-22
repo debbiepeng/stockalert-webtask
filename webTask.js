492668317116cc304295e867cd342727c15249bf
debbiepeng <debbiepeng@gmail.com>
2017-05-21 23:39:26 -0400

    use google finance for historical prices too, because Yahoo stopped providing quotes

--- webTask.js
+++ webTask.js
@@ -8,6 +8,7 @@ const Mailgun = require("mailgun-js");
 const moment = require("moment");
 //const fincal = require("fincal"); 
 const business = require("moment-business");
+const googleFinance = require("google-finance");
 
 var getIntradayStockQuote = function (ticker) {
   let url = `http://finance.google.com/finance/info?q=${ticker}`;
@@ -19,25 +20,30 @@ var getIntradayStockQuote = function (ticker) {
 };
 
 var getHistoricalPrices = function (ticker) {
-  let endDate = moment();
-  let startDate = moment(endDate).subtract(1, "month");
-  let url = `https://query.yahooapis.com/v1/public/yql?q=select Date, High, Low, Close from yahoo.finance.historicaldata where symbol in ("${ticker}")
-  and startDate="${startDate.format("YYYY-MM-DD")}" and endDate="${endDate.format("YYYY-MM-DD")}"&format=json&env=store://datatables.org/alltableswithkeys`;  
-  return fetch(url)
-    .then(res => res.json())
-    .catch(err => {
-      throw new Error(`Cannot get historical prices: ${err.message}.`);
-    });
+  let endDate = moment().toDate();
+  let startDate = moment(endDate).subtract(1, "month").toDate();
+  
+  return googleFinance.historical({
+    symbol: ticker,
+    from: startDate,
+    to: endDate
+  })
+  .then(quotes => {    
+    return quotes;
+  })
+  .catch(err => {
+    throw new Error(`Cannot get historical prices: ${err.message}.`);
+  });
 }
 
 var calculateSupportResistenceLevels = function (historicalPrices) {
   // We are assuming stock highs and lows are lognorminal distributed, 
   // and we calculate the 95% confidence interval to get an estimate of the upper and lower bounds of stock prices
-  let historicalLows = _.map(historicalPrices, (q => Math.log(parseFloat(q.Low))));
+  let historicalLows = _.map(historicalPrices, (q => Math.log(parseFloat(q.low))));
   let statistics = confidence95(historicalLows);
   let support = Math.exp(statistics.mean - statistics.interval);
 
-  let historicalHighs = _.map(historicalPrices, (q => Math.log(parseFloat(q.High))));
+  let historicalHighs = _.map(historicalPrices, (q => Math.log(parseFloat(q.high))));
   statistics = confidence95(historicalHighs);
   var resistence = Math.exp(statistics.mean + statistics.interval);
 
@@ -56,19 +62,18 @@ var callMailgun = function (subject, text, cb) {
     text
   };
 
-  var sendPromise = P.promisify(mailgun.messages().send, { context: mailgun.messages() });  
-  return sendPromise(data)    
+  var sendPromise = P.promisify(mailgun.messages().send, { context: mailgun.messages() });
+  return sendPromise(data)
     .catch(error => {
       cb(null, `Called Mailgun: ${JSON.stringify(error)}`);
     });;
 }
 
 var checkPricesAPI = function (ctx, cb) {
-  P.join(getIntradayStockQuote(ctx.data.ticker), getHistoricalPrices(ctx.data.ticker),  
-    (intradayPriceJson, historicalPriceJson) => {
-      intradayPriceJson = intradayPriceJson.replace("//", "");      
+  P.join(getIntradayStockQuote(ctx.data.ticker), getHistoricalPrices(ctx.data.ticker),
+    (intradayPriceJson, historicalPrices) => {
+      intradayPriceJson = intradayPriceJson.replace("//", "");
       var intradayPrice = JSON.parse(intradayPriceJson)[0].l;      
-      var historicalPrices = _.map(historicalPriceJson.query.results.quote, q => q);
       var calcResults = calculateSupportResistenceLevels(historicalPrices);
       let mailBody = `Estimated Support Level: ${calcResults.support}\nEstimated Resistence Level: ${calcResults.resistence}\nLatest Price: ${intradayPrice}\n`;
       if (intradayPrice >= calcResults.resistence || intradayPrice <= calcResults.support) {
@@ -81,7 +86,7 @@ var checkPricesAPI = function (ctx, cb) {
         let isBusinessHour = business.isWeekDay(now) && now.format("HH:mm").isBetween("09:30", "16:00", "HH:mm");
         if (isBusinessHour) {
           callMailgun(subject, mailBody + actionRecommendation, cb);
-        }        
+        }
       }
       else {
         cb(null, `Nothing exciting to report.\n${mailBody}`);
